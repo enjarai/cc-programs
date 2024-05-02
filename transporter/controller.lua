17098a5a2884eda8bda044c5e3ff1e63ea4eeaef
enjarai <enjarai123@gmail.com>
2024-05-02 14:29:43 +0200

    hm?

--- transporter/controller.lua
+++ transporter/controller.lua
@@ -20,6 +20,11 @@ while true do
     local input = read(nil, history)
     table.insert(history, input)
 
+    if input == "here" then
+        local x, y, z = gps.locate()
+        input = ("target;%d;%d;%d"):format(x, y + 10, z)
+    end
+
     rednet.send(id, input, PROTOCOL)
     local _, message, _ = rednet.receive(PROTOCOL)
     print(message)
